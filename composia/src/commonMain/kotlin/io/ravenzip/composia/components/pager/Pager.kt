dcb2484ad4bd80049b455020ec7f8ce7252e59d9
Черных Александр <razor01478920@mail.ru>
2025-07-27 17:12:02 +0300

    Добавлены вертикальный и горизонтальный пейджеры с индикаторами

--- composia/src/commonMain/kotlin/io/ravenzip/composia/components/pager/Pager.kt
+++ composia/src/commonMain/kotlin/io/ravenzip/composia/components/pager/Pager.kt
@@ -1,4 +1,37 @@
 package io.ravenzip.composia.components.pager
 
-// TODO HorizontalPager
-// TODO VerticalPager
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerScope
+import androidx.compose.foundation.pager.PagerState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun VerticalHPagerWithIndicator(
+    pagerState: PagerState,
+    pageContent: @Composable (PagerScope.(Int) -> Unit),
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        HorizontalPager(state = pagerState, modifier = Modifier.fillMaxSize()) { content ->
+            pageContent(content)
+        }
+
+        Box(modifier = Modifier.fillMaxSize()) { VerticalPagerIndicator(pagerState) }
+    }
+}
+
+@Composable
+fun HorizontalPagerWithIndicator(
+    pagerState: PagerState,
+    pageContent: @Composable (PagerScope.(Int) -> Unit),
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        HorizontalPager(state = pagerState, modifier = Modifier.fillMaxSize()) { content ->
+            pageContent(content)
+        }
+
+        Box(modifier = Modifier.fillMaxSize()) { HorizontalPagerIndicator(pagerState) }
+    }
+}
