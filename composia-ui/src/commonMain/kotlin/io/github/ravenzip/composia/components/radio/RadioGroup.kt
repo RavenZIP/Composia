65f206e9d29efc91089a2c7684e2ffd7014773e5
Черных Александр <razor01478920@mail.ru>
2025-08-22 08:47:06 +0300

    Добавлена радиогруппа

---
+++ composia-ui/src/commonMain/kotlin/io/github/ravenzip/composia/components/radio/RadioGroup.kt
@@ -0,0 +1,55 @@
+package io.github.ravenzip.composia.components.radio
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import io.github.ravenzip.composia.control.formControl.FormControl
+
+@Composable
+fun <T, K> RadioGroup(
+    control: FormControl<T>,
+    source: List<T>,
+    view: (T) -> String,
+    keySelector: (T) -> K,
+    modifier: Modifier = Modifier.fillMaxWidth(0.9f),
+    textStyle: TextStyle = LocalTextStyle.current,
+    contentPadding: Arrangement.Vertical = Arrangement.spacedBy(10.dp),
+    colors: RadioButtonColors = RadioButtonDefaults.colors(),
+) {
+    val value = control.valueFlow.collectAsState().value
+    val selectedKey = remember(value) { keySelector(control.value) }
+
+    Column(modifier = modifier, verticalArrangement = contentPadding) {
+        source.forEach { item ->
+            Row(
+                modifier =
+                    Modifier.fillMaxWidth()
+                        .clip(RoundedCornerShape(10.dp))
+                        .clickable { control.setValue(item) }
+                        .padding(top = 5.dp, bottom = 5.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                val text = remember(item) { view(item) }
+                val key = remember(item) { keySelector(item) }
+
+                RadioButton(
+                    selected = selectedKey == key,
+                    onClick = { control.setValue(item) },
+                    enabled = control.isEnabled,
+                    colors = colors,
+                )
+
+                Text(text = text, style = textStyle)
+            }
+        }
+    }
+}
