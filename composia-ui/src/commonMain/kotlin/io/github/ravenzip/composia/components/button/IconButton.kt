a3a43c259a6c677c2a9d5470095fc0af6ac872fa
Черных Александр <razor01478920@mail.ru>
2025-08-23 09:20:49 +0300

    Реализация enabled\disabled для IconButton

--- composia-ui/src/commonMain/kotlin/io/github/ravenzip/composia/components/button/IconButton.kt
+++ composia-ui/src/commonMain/kotlin/io/github/ravenzip/composia/components/button/IconButton.kt
@@ -1,36 +1,47 @@
 package io.github.ravenzip.composia.components.button
 
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.semantics.Role
+import androidx.compose.ui.semantics.role
+import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.unit.dp
-import io.github.ravenzip.composia.components.layout.RoundedBox
 import io.github.ravenzip.composia.control.statusControl.StatusControl
 import io.github.ravenzip.composia.style.IconStyle
 
-// TODO реализовать enabled\disabled
 @Composable
 fun IconButton(
     icon: Painter,
     iconDescription: String? = null,
     iconStyle: IconStyle = IconStyle.Default,
-    backgroundColor: Color,
+    modifier: Modifier = Modifier,
+    isEnabled: Boolean = true,
     shape: Shape = RoundedCornerShape(10.dp),
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    interactionSource: MutableInteractionSource? = null,
     onClick: () -> Unit,
 ) {
+    val interactionSource = interactionSource ?: remember { MutableInteractionSource() }
     val color = iconStyle.color ?: MaterialTheme.colorScheme.primary
+    val containerColor = if (isEnabled) colors.containerColor else colors.disabledContainerColor
+    val contentColor = if (isEnabled) colors.contentColor else colors.disabledContentColor
 
-    RoundedBox(
-        modifier = Modifier.clickable { onClick() },
-        backgroundColor = backgroundColor,
+    Surface(
+        onClick = onClick,
+        modifier = modifier.semantics { role = Role.Button },
+        enabled = isEnabled,
         shape = shape,
+        color = containerColor,
+        contentColor = contentColor,
+        interactionSource = interactionSource,
     ) {
         Icon(
             painter = icon,
@@ -41,29 +52,28 @@ fun IconButton(
     }
 }
 
-// TODO реализовать enabled\disabled с использованием control
 @Composable
 fun IconButton(
     control: StatusControl,
+    modifier: Modifier = Modifier,
     icon: Painter,
     iconDescription: String? = null,
     iconStyle: IconStyle = IconStyle.Default,
-    backgroundColor: Color,
     shape: Shape = RoundedCornerShape(10.dp),
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
     onClick: () -> Unit,
 ) {
-    val color = iconStyle.color ?: MaterialTheme.colorScheme.primary
+    val modifiedColors = colors.copy(contentColor = iconStyle.color ?: colors.contentColor)
+    val isEnabled = control.isEnabledFlow.collectAsState().value
 
-    RoundedBox(
-        modifier = Modifier.clickable { onClick() },
-        backgroundColor = backgroundColor,
+    IconButton(
+        icon = icon,
+        iconDescription = iconDescription,
+        iconStyle = iconStyle,
+        modifier = modifier,
+        isEnabled = isEnabled,
         shape = shape,
-    ) {
-        Icon(
-            painter = icon,
-            contentDescription = iconDescription,
-            modifier = Modifier.size(iconStyle.size.dp),
-            tint = color,
-        )
-    }
+        colors = modifiedColors,
+        onClick = onClick,
+    )
 }
