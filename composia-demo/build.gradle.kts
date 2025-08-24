08507b3ee4ba786d91ba10a0dee390b7aada5f17
Черных Александр <razor01478920@mail.ru>
2025-08-24 20:32:43 +0300

    Превью плагин ломал сборку на wasm, потому что ходил за зависимостями по пути org.jetbrains.compose.ui:ui-tooling-preview, а должен был в org.jetbrains.compose.components:components-ui-tooling-preview или с припиской wasmJs

--- composia-demo/build.gradle.kts
+++ composia-demo/build.gradle.kts
@@ -49,7 +49,6 @@ kotlin {
             implementation(compose.foundation)
             implementation(compose.material3)
             implementation(compose.ui)
-            implementation(compose.preview)
             implementation(compose.components.resources)
 
             implementation(libs.androidx.viewmodel.viewmodel)
--- composia-demo/src/commonMain/kotlin/io/github/ravenzip/composia/App.kt
+++ composia-demo/src/commonMain/kotlin/io/github/ravenzip/composia/App.kt
@@ -7,10 +7,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.rememberNavController
 import io.github.ravenzip.composia.navigation.NavigationGraph
-import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
-@Preview
 fun App() {
     val navController = rememberNavController()
 
