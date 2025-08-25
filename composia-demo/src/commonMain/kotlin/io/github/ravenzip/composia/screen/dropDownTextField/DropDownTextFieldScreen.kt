12f6c0c8381ac1bac990c3df9f155f19fd0afe50
Черных Александр <razor01478920@mail.ru>
2025-08-25 17:09:57 +0300

    Основная потеряшка с прошлого коммита

---
+++ composia-demo/src/commonMain/kotlin/io/github/ravenzip/composia/screen/dropDownTextField/DropDownTextFieldScreen.kt
@@ -0,0 +1,71 @@
+package io.github.ravenzip.composia.screen.dropDownTextField
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import io.github.ravenzip.composia.components.button.SimpleButton
+import io.github.ravenzip.composia.components.textField.dropdown.DropDownTextField
+import io.github.ravenzip.composia.control.compositeControl.CompositeControl
+import io.github.ravenzip.composia.sample.Item
+import io.github.ravenzip.composia.state.DropDownTextFieldState
+
+@Composable
+fun DropDownTextFieldScreen(padding: PaddingValues, backToMenu: () -> Unit) {
+    val scope = rememberCoroutineScope()
+    val control = remember {
+        CompositeControl(initialValue = Item.createEmptyModel(), coroutineScope = scope)
+    }
+    val state = remember { DropDownTextFieldState() }
+    val source = remember { Item.createItems() }
+
+    Box(modifier = Modifier.fillMaxSize().padding(padding), contentAlignment = Alignment.Center) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(10.dp),
+        ) {
+            DropDownTextField(
+                control = control,
+                state = state,
+                source = source,
+                sourceItemToString = { x -> x.name },
+            )
+
+            DropDownTextFieldInfo(control = control, state = state)
+
+            SimpleButton(onClick = { backToMenu() }, text = "Back to menu")
+        }
+    }
+}
+
+@Composable
+fun DropDownTextFieldInfo(control: CompositeControl<Item>, state: DropDownTextFieldState) {
+    val controlSnapshot = control.snapshotFlow.collectAsState().value
+    val isReadonly = state.readonlyState.valueFlow.collectAsState().value
+    val isFucused = state.focusedState.valueFlow.collectAsState().value
+    val isExpanded = state.expandedState.valueFlow.collectAsState().value
+
+    Card(modifier = Modifier.fillMaxWidth(0.9f)) {
+        Column(modifier = Modifier.padding(10.dp)) {
+            Text("Выбранное значение: ${controlSnapshot.value.name.ifEmpty { "Пусто" }}")
+            Text("Последний тип изменений: ${controlSnapshot.typeChange}")
+            Text("Были изменения: ${ if(controlSnapshot.hasChanges) "Да" else "Нет"}")
+            Text("Включено: ${if (controlSnapshot.isEnabled) "Да" else "Нет"}")
+            Text("Валидно: ${if (controlSnapshot.isValid) "Да" else "Нет"}")
+
+            if (controlSnapshot.isValid) {
+                Text("Текст ошибки: ${controlSnapshot.errorMessage}")
+            }
+
+            Text("Только для чтения: ${if (isReadonly) "Да" else "Нет"}")
+            Text("Открыто: ${if (isExpanded) "Да" else "Нет"}")
+            Text("В фокусе: ${if (isFucused) "Да" else "Нет"}")
+        }
+    }
+}
