281deac0e63ea4c5078f201173cc12c4821ed31c
Черных Александр <razor01478920@mail.ru>
2025-08-25 17:10:20 +0300

    Несколько перегрузок для RichButton (аналогично другим компонентам)

--- composia-ui/src/commonMain/kotlin/io/github/ravenzip/composia/components/button/RichButton.kt
+++ composia-ui/src/commonMain/kotlin/io/github/ravenzip/composia/components/button/RichButton.kt
@@ -19,6 +19,116 @@ import io.github.ravenzip.composia.style.IconStyle
 
 @Composable
 fun RichButton(
+    onClick: () -> Unit = {},
+    modifier: Modifier = Modifier,
+    label: @Composable () -> Unit,
+    description: @Composable () -> Unit,
+    icon: @Composable () -> Unit,
+    isEnabled: Boolean = true,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    shape: Shape = RoundedCornerShape(10.dp),
+    contentPadding: PaddingValues = PaddingValues(18.dp),
+) {
+    Button(
+        onClick = onClick,
+        modifier = modifier,
+        enabled = isEnabled,
+        colors = colors,
+        shape = shape,
+        contentPadding = contentPadding,
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(15.dp),
+        ) {
+            icon()
+
+            Column(
+                modifier = Modifier.fillMaxWidth(),
+                verticalArrangement = Arrangement.spacedBy(5.dp),
+            ) {
+                label()
+
+                description()
+            }
+        }
+    }
+}
+
+@Composable
+fun RichButton(
+    onClick: () -> Unit = {},
+    modifier: Modifier = Modifier,
+    label: String,
+    labelStyle: TextStyle = TextStyle.S18Medium,
+    description: String,
+    descriptionStyle: TextStyle = TextStyle.S20,
+    icon: Painter,
+    iconDescription: String? = null,
+    iconStyle: IconStyle = IconStyle.Default,
+    isEnabled: Boolean = true,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    shape: Shape = RoundedCornerShape(10.dp),
+    contentPadding: PaddingValues = PaddingValues(18.dp),
+) {
+    val labelColor =
+        if (labelStyle.color != Color.Unspecified) labelStyle.color else colors.contentColor
+    val descriptionColor =
+        if (descriptionStyle.color != Color.Unspecified) descriptionStyle.color
+        else colors.contentColor
+
+    val mergedLabelStyle = labelStyle.merge(color = labelColor)
+    val mergedDescriptionStyle = descriptionStyle.merge(color = descriptionColor)
+
+    RichButton(
+        onClick = onClick,
+        modifier = modifier,
+        label = { Text(text = label, style = mergedLabelStyle) },
+        description = { Text(text = description, style = mergedDescriptionStyle) },
+        icon = {
+            Icon(
+                painter = icon,
+                contentDescription = iconDescription,
+                modifier = Modifier.size(iconStyle.size.dp),
+                tint = iconStyle.color ?: colors.contentColor,
+            )
+        },
+        isEnabled = isEnabled,
+        colors = colors,
+        shape = shape,
+        contentPadding = contentPadding,
+    )
+}
+
+@Composable
+fun RichButton(
+    control: StatusControl,
+    modifier: Modifier = Modifier,
+    label: @Composable () -> Unit,
+    description: @Composable () -> Unit,
+    icon: @Composable () -> Unit,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    shape: Shape = RoundedCornerShape(10.dp),
+    contentPadding: PaddingValues = PaddingValues(18.dp),
+    onClick: () -> Unit = {},
+) {
+    val isEnabled = control.isEnabledFlow.collectAsState().value
+
+    RichButton(
+        onClick = onClick,
+        modifier = modifier,
+        label = label,
+        description = description,
+        icon = icon,
+        isEnabled = isEnabled,
+        colors = colors,
+        shape = shape,
+        contentPadding = contentPadding,
+    )
+}
+
+@Composable
+fun RichButton(
     control: StatusControl,
     modifier: Modifier = Modifier,
     label: String,
@@ -44,72 +154,19 @@ fun RichButton(
     val mergedLabelStyle = labelStyle.merge(color = labelColor)
     val mergedDescriptionStyle = descriptionStyle.merge(color = descriptionColor)
 
-    Button(
-        onClick = { onClick() },
+    RichButton(
+        onClick = onClick,
         modifier = modifier,
-        enabled = isEnabled,
+        label = label,
+        labelStyle = mergedLabelStyle,
+        description = description,
+        descriptionStyle = mergedDescriptionStyle,
+        icon = icon,
+        iconDescription = iconDescription,
+        iconStyle = iconStyle,
+        isEnabled = isEnabled,
         colors = colors,
         shape = shape,
         contentPadding = contentPadding,
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(15.dp),
-        ) {
-            Icon(
-                painter = icon,
-                contentDescription = iconDescription,
-                modifier = Modifier.size(iconStyle.size.dp),
-                tint = iconStyle.color ?: colors.contentColor,
-            )
-
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(5.dp),
-            ) {
-                Text(text = label, style = mergedLabelStyle)
-                Text(text = description, style = mergedDescriptionStyle)
-            }
-        }
-    }
-}
-
-@Composable
-fun RichButton(
-    control: StatusControl,
-    modifier: Modifier = Modifier,
-    label: @Composable () -> Unit,
-    description: @Composable () -> Unit,
-    icon: @Composable () -> Unit,
-    colors: ButtonColors = ButtonDefaults.buttonColors(),
-    shape: Shape = RoundedCornerShape(10.dp),
-    contentPadding: PaddingValues = PaddingValues(18.dp),
-    onClick: () -> Unit = {},
-) {
-    val isEnabled = control.isEnabledFlow.collectAsState().value
-
-    Button(
-        onClick = { onClick() },
-        modifier = modifier,
-        enabled = isEnabled,
-        colors = colors,
-        shape = shape,
-        contentPadding = contentPadding,
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(15.dp),
-        ) {
-            icon()
-
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                verticalArrangement = Arrangement.spacedBy(5.dp),
-            ) {
-                label()
-
-                description()
-            }
-        }
-    }
+    )
 }
